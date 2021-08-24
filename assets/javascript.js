fb64ac0475390717cd04158efb73930f6bf12722
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-08-24 16:44:47 -0500

    figured out why previous attemps did not work

---
+++ assets/javascript.js
@@ -0,0 +1,15 @@
+var buttonEl = document.querySelector("#save-task"); 
+
+buttonEl.addEventListener("click", function() {
+    alert("button clicked");
+  });
+// var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+
+// var createTaskHandler = function() { 
+//   var listItemEl = document.createElement("li"); 
+//   listItemEl.className = "task-item"; 
+//   listItemEl.textContent = "This is a new task."; 
+//   tasksToDoEl.appendChild(listItemEl); 
+//   }; 
+
+// buttonEl.addEventListener("click", createTaskHandler);
