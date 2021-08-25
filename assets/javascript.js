020f9b799c4cb0dd2155120d3ecc3b6b12b62ad3
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-08-25 18:56:18 -0500

    add-task complete

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,15 +1,23 @@
 var buttonEl = document.querySelector("#save-task"); 
+var taskItemEl = document.createElement('li');
+var taskToDoEl = document.querySelector('#tasks-to-do');
 
-buttonEl.addEventListener("click", function() {
-    alert("button clicked");
-  });
-// var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-// var createTaskHandler = function() { 
-//   var listItemEl = document.createElement("li"); 
-//   listItemEl.className = "task-item"; 
-//   listItemEl.textContent = "This is a new task."; 
-//   tasksToDoEl.appendChild(listItemEl); 
-//   }; 
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  }
 
-// buttonEl.addEventListener("click", createTaskHandler);
+buttonEl.addEventListener("click", createTaskHandler);
+
+
+
+
+
+
+
+
+
+
